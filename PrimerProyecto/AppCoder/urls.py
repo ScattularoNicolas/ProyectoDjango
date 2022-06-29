79c624500a189acd0d562e68c1f744750381d6ed
scattularo <snscattularo@hotmail.com>
2022-06-28 23:34:53 -0300

    se agregan los statics

--- PrimerProyecto/AppCoder/urls.py
+++ PrimerProyecto/AppCoder/urls.py
@@ -5,8 +5,8 @@ from AppCoder.views import *
 from . import views
 
 urlpatterns = [
-    path("DisiplinasDeportivas", views.disciplinasDeportivas),
-    path("Profesores",views.profesores),
-    path("Alumnos",views.alumnos),
-    path("Sedes",views.sedes),
+    path("DisiplinasDeportivas", views.disciplinasDeportivas, name='disciplinasDeportivas'),
+    path("Profesores", views.profesores, name='profesores'),
+    path("Alumnos", views.alumnos, name='alumnos'),
+    path("Sedes", views.sedes, name='sedes'),
 ]
